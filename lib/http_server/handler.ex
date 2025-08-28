3564bb41448ea04c7cdc600c808bdd6f0df1dff8
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-29 00:03:38 +1000

    fix: dynamic status and status reason in response

--- lib/http_server/handler.ex
+++ lib/http_server/handler.ex
@@ -14,31 +14,46 @@ defmodule HttpServer.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
   end
+
+  def log(conv), do: IO.inspect(conv)
 
   def route(conv) do
     conv |> route(conv.method, conv.path)
   end
 
-  def log(conv), do: IO.inspect(conv)
-
   def route(conv, "GET", "/hello") do
-    %{conv | resp_body: "hello world!"}
+    %{conv | resp_body: "hello world!", status: 200}
   end
 
   def route(conv, method, path) do
-    %{conv | resp_body: "No endpoints for #{method} #{path}"}
+    %{conv | resp_body: "No endpoints for #{method} #{path}", status: 404}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
