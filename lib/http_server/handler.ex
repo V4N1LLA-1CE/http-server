66be0470d2bd83225eb92a709227400f4d7f3e01
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-28 23:49:32 +1000

    feat: create handle methods and routes with fallbacks

--- lib/http_server/handler.ex
+++ lib/http_server/handler.ex
@@ -2,20 +2,43 @@ defmodule HttpServer.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
 
   def parse(conv) do
-    # TODO: parse request string into map
+    [method, path, _http_version] =
+      conv
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
-    # TODO: what does this specific route do? change response body?
+    conv |> route(conv.method, conv.path)
+  end
+
+  def log(conv), do: IO.inspect(conv)
+
+  def route(conv, "GET", "/hello") do
+    %{conv | resp_body: "hello world!"}
+  end
+
+  def route(conv, method, path) do
+    %{conv | resp_body: "No endpoints for #{method} #{path}"}
   end
 
   def format_response(conv) do
-    # TODO: format response to a http response string
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
   end
 end
 
@@ -26,10 +49,5 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 """
 
-sample_response = """
-HTTP/1.1 200 OK
-Content-Type: text/html
-Content-Length: 20
-
-Hello World!
-"""
+response = HttpServer.Handler.handle(sample_request)
+IO.puts(response)
