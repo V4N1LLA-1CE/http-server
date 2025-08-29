1ea61e13a0381ebfb3338561f959d637efbb21f1
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-29 19:46:19 +1000

    feat: module docs and attributes

--- lib/http_server/handler.ex
+++ lib/http_server/handler.ex
@@ -1,4 +1,13 @@
 defmodule HttpServer.Handler do
+  @moduledoc """
+  Handles HTTP requests.
+  """
+
+  @pages_path Path.expand("../pages", __DIR__)
+
+  @doc """
+  Transforms the request into a response.
+  """
   def handle(request) do
     request
     |> parse
@@ -24,7 +33,7 @@ defmodule HttpServer.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../pages", __DIR__)
+    @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
