fc55fe12265a9d15715674eb0eb9a6192c98f31b
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-29 18:48:52 +1000

    refactor: multiclause function

--- lib/http_server/handler.ex
+++ lib/http_server/handler.ex
@@ -24,20 +24,10 @@ defmodule HttpServer.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    file =
-      Path.expand("../pages", __DIR__)
-      |> Path.join("about.html")
-
-    case File.read(file) do
-      {:ok, content} ->
-        %{conv | status: 200, resp_body: content}
-
-      {:error, :enoent} ->
-        %{conv | status: 404, resp_body: "Page does not exist or cannot be found"}
-
-      {:error, reason} ->
-        %{conv | status: 500, resp_body: "File error: #{reason}"}
-    end
+    Path.expand("../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
   end
 
   def route(%{method: "GET", path: "/resource" <> id} = conv) do
@@ -46,6 +36,18 @@ defmodule HttpServer.Handler do
 
   def route(%{method: method, path: path} = conv) do
     %{conv | status: 404, resp_body: "No endpoint for #{method} #{path}"}
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "Page does not exist or cannot be found"}
+  end
+
+  def handle_file({:ok, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   def format_response(conv) do
