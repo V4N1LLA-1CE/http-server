ff30ac01fed97996abe4ece02d2035f6593f1668
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-31 01:49:05 +1000

    feat: recursion through params map

--- lib/http_server/parser.ex
+++ lib/http_server/parser.ex
@@ -3,22 +3,36 @@ defmodule HttpServer.Parser do
 
   def parse(request) do
     [top, param_string] = String.split(request, "\n\n")
-    [start | _header] = top |> String.split("\n")
+    [start | header] = top |> String.split("\n")
 
-    params = parse_params(param_string)
+    headers = parse_headers(header, %{})
+    params = parse_params(headers["Content-Type"], param_string)
 
     [method, path, _http_version] = start |> String.split(" ")
 
     %Conv{
       method: method,
       path: path,
-      params: params
+      params: params,
+      headers: headers
     }
   end
 
-  def parse_params(param_string) do
+  def parse_params("application/x-www-form-urlencoded", param_string) do
     param_string
     |> String.trim()
     |> URI.decode_query()
   end
+
+  def parse_params(_, _), do: %{}
+
+  def parse_headers([head | tail], headers) do
+    [key, value] = head |> String.split(": ")
+
+    headers = Map.put(headers, key, value)
+
+    parse_headers(tail, headers)
+  end
+
+  def parse_headers([], headers), do: headers
 end
